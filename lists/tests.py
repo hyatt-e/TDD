743110cc6f943a8df78ed20e2aebdbd55e32f265
Eric <hyatt1ea@gmail.com>
2019-04-11 19:59:43 -0400

    Added HTML for front page using template.

--- lists/tests.py
+++ lists/tests.py
@@ -8,14 +8,15 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        # resolve is the function Django uses internally to resolve
-        # URLs and find what view function they should map to
-        # We’re checking that resolve, when called with “/”, the root
-        # of the site, finds a function called home_page
+    # this is tested when the Djando Test Client is used
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     # resolve is the function Django uses internally to resolve
+    #     # URLs and find what view function they should map to
+    #     # We’re checking that resolve, when called with “/”, the root
+    #     # of the site, finds a function called home_page
 
-        self.assertEqual(found.func, home_page)
+    #     self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         # request = HttpRequest()
@@ -33,17 +34,17 @@ class HomePageTest(TestCase):
         # raw bytes, the ones and zeros that would be sent down the 
         # wire to the user’s browser. We call .decode() to convert 
         # them into the string of HTML that’s being sent to the user.
-        html = response.content.decode("utf8")
+        # html = response.content.decode("utf8")
 
-        self.assertTrue(html.startswith('<html>'))
+        # self.assertTrue(html.startswith('<html>'))
         # We want it to start with an <html> tag which gets closed
         # at the end.
 
-        self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertIn('<title>To-Do lists</title>', html)
         # And we want a <title> tag somewhere in the middle, with the words
         # "To-Do lists" in it—​because that’s what we specified in our functional test.
 
-        self.assertTrue(html.strip().endswith('</html>'))
+        # self.assertTrue(html.strip().endswith('</html>'))
         #   We want it to start with an <html> tag which gets closed
         #   at the end.
         # .strip() makes sure there's no whitespace at the end of the file
