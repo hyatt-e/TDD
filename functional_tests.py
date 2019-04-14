e7ca52c3aef0e908014633ceb75bb09e9d4ccd88
Eric <hyatt1ea@gmail.com>
2019-04-14 19:13:57 -0400

    Refactored FT test_can_start_a_list_and_retrieve_it_later

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # will not be run by test since it doesn't begin with test_
+    def check_for_row_in_list_table(self, row_text):
+        # set table = to variable
+        table = self.browser.find_element_by_id("id_list_table")
+        # find rows of table
+        rows = table.find_elements_by_tag_name('tr')
+        # loop through rows to check for text
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -40,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers" , [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to make a fly" , [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
