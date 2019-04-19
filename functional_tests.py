3bf7b96a3a1453d6ee1bcb4459788915eff26312
Eric <hyatt1ea@gmail.com>
2019-04-19 19:25:03 -0400

    Redirect after POST and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -48,8 +48,13 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
-
         self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # There is still a text box inviting her to add another item. She
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # No londer need HttpResponse since we're handling HTTP with a template
 from django.http import HttpResponse
 from .models import Item
@@ -6,13 +6,10 @@ from .models import Item
 
 def home_page(request):
     if request.method == "POST":
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect("/")
+
+    items = Item.objects.all()
     # Dj auto searches for the second argument inside any folders named
     # templates that are inside of one of your apps folders
-    return render(request, 'home.html', {
-        # use <dict>.get to supply a default value when POST is blank
-        "new_item_text": new_item_text,
-        })
+    return render(request, 'home.html', {'items': items})
