2594eccc7c3bb87351b440adefca2050c6dcd306
Eric <hyatt1ea@gmail.com>
2019-04-19 20:45:00 -0400

    Refactored functional tests to remove voodoo sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,15 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
+MAX_WAIT = 10
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
+
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,18 +18,30 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     # will not be run by test since it doesn't begin with test_
-    def check_for_row_in_list_table(self, row_text):
-        # set table = to variable
-        table = self.browser.find_element_by_id("id_list_table")
-        # find rows of table
-        rows = table.find_elements_by_tag_name('tr')
-        # loop through rows to check for text
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:                
+                # set table = to variable
+                table = self.browser.find_element_by_id("id_list_table")
+                # find rows of table
+                rows = table.find_elements_by_tag_name('tr')
+                # loop through rows to check for text
+                self.assertIn(row_text, [row.text for row in rows])
+                # if we get throuhg the asserts, we return the function and exit the loop
+                return
+            # WebDriverException for when the page hasn’t loaded and Selenium can’t find the table
+            # element on the page, and AssertionError for when the table is there, but it’s perhaps a
+            # table from before the page reloads, so it doesn’t have our row in yet.
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -47,15 +64,13 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
 
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -69,8 +84,4 @@ class NewVisitorTest(unittest.TestCase):
 
         # She visits that URL - her to-do list is still there.
 
-        # Satisfied, she goes back to sleep
-
-if __name__ == "__main__":
-    # unittest.main(warnings="ignore")
-    unittest.main()
+        # Satisfied, she goes back to sleepk
