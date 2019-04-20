6ccb6a45e1f3e99eff71489e617436146f56c0bf
Eric <hyatt1ea@gmail.com>
2019-04-20 18:27:43 -0400

    Add FT to check functionality for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,7 +38,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
@@ -74,7 +74,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail("Finish the test!")
+    # self.fail("Finish the test!")
 
         # The page updates again, and now shows both items on her list
 
@@ -84,4 +84,41 @@ class NewVisitorTest(LiveServerTestCase):
 
         # She visits that URL - her to-do list is still there.
 
-        # Satisfied, she goes back to sleepk
+        # Satisfied, she goes back to sleep
+    
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith satrts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+
+        # she notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now a new user, Francis, comes to the site
+        ## We use a new browser session to make sure that no information is
+        #coming throuhg from cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        # Francis starts a new list by entering a new item.
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1. Buy milk")
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+
+        # Satisfied, they both go back to sleep
