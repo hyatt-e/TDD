077ee8555079cff4c18c958cc2b8eae5150f1a8b
Eric <hyatt1ea@gmail.com>
2019-04-28 19:52:21 -0400

    Added 'action' URL for adding a list

--- lists/tests.py
+++ lists/tests.py
@@ -54,23 +54,6 @@ class HomePageTest(TestCase):
         # .assertTemplateUsed is the test method Django TestCase class provides us.
         # it lets us check what template was used to render a response 
         # (NB - it will only work for responses that were retrieved by the test client)
-        
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={"item_text": "A new list item"})
-
-        # check that an new item was saved to database
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={"item_text": "A new list item"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], "/lists/the-only-list-in-the-world/")
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
@@ -107,3 +90,18 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new', data={"item_text": "A new list item"})
+
+        # check that an new item was saved to database
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,6 @@ from .models import Item
 
 
 def home_page(request):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect("/lists/the-only-list-in-the-world/")
-
-    items = Item.objects.all()
     # Dj auto searches for the second argument inside any folders named
     # templates that are inside of one of your apps folders
     return render(request, 'home.html')
@@ -17,3 +12,7 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
