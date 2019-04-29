8815c65228dba56d613d7973647ac4c47e2792e4
Eric <hyatt1ea@gmail.com>
2019-04-28 21:48:15 -0400

    Created new url for adding items to a list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
-MAX_WAIT = 5
+MAX_WAIT = 1
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -114,7 +114,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Buy milk")
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1. Buy milk")
+        self.wait_for_row_in_list_table("1: Buy milk")
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
