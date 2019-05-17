740e12ebc8fe12b58857ad62f92237ccfdc97381
Eric <hyatt1ea@gmail.com>
2019-05-16 21:59:47 -0400

    Added bootstrap css

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -8,7 +9,7 @@ import unittest
 MAX_WAIT = 1
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 
     def setUp(self):
